c336678201b5c3a9c867151e4c2a81de6e0e5786
ayush6624 <ayushg1214@gmail.com>
2019-12-21 22:29:13 +0530

    removed debugging and removed alert in js

--- assets/public.js
+++ assets/public.js
@@ -26,7 +26,7 @@ client.get(theurl, response => {
   // alert(window.parkingLots);
   window.parkingLots = JSON.parse(response);
   // alert(parkingLots[0].latitude);
-  alert(window.parkingLots);
+  // alert(window.parkingLots);
   var map = new MapmyIndia.Map('mainMap', { center: [28.61, 77.23], zoomControl: true, hybrid: true, search: true, location: true }); /*map initialized*/
 
   map.setZoom(11);
@@ -37,45 +37,65 @@ client.get(theurl, response => {
       iconSize: [10, 10],
       popupAnchor: [12, -10]
     });
-    console.log(typeof parkingLots[index].latitude);
+    // console.log(typeof parkingLots[index].latitude);
     var mk = addMarker({
       position: [parkingLots[index].latitude, parkingLots[index].longitude],
       title: 'hi',
       draggable: false,
-      icon: icon
+      icon: icon,
+      obj: parkingLots[index]
     });
   }
 
   function test() {
     document.getElementById('test').innerHTML = console.log(response);
   }
-
+  function create_content(title, content) {
+    var h = new Array();
+    h.push('<div>');
+    h.push('<div class="header">');
+    h.push('<span>');
+    h.push(title);
+    h.push('</span> ');
+    h.push('</div>');
+    h.push('<div class="info_css">');
+    h.push(content);
+    h.push('</div>');
+    h.push('</div>');
+    return h.join('');
+  }
   function addMarker(req) {
+    console.log('I');
     var mk = new L.marker(req.position, {
       draggable: req.draggable,
       icon: req.icon,
       title: req.title
     });
-    mk.bindPopup(req.title);
+    // mk.bindPopup(req.title);
+    var content = create_content('Info', 'Available space: ' + req.obj.available_space + '<br>' + 'Cost/hr: ' + req.obj.cost_per_hour);
+    mk.bindPopup(content);
+    mk.on('click', () => {
+      mk.openPopup();
+    });
     map.addLayer(mk);
     return mk;
   }
 
-  map.on('click', function(e) {
-    var pt = e.latlng;
-    document.getElementById('test').innerHTML = pt;
-    var icon = L.divIcon({
-      className: 'my-div-icon',
-      // html:'<div class="popper">' + index+1 + '</div>',
-      html: "<img class='popper'  src=" + "'https://maps.mapmyindia.com/images/2.png'>" + '<span class="my-div-span">' + (index + 1) + '</span>',
-      iconSize: [2, 10],
-      popupAnchor: [12, -10]
-    });
-    var mk = addMarker({
-      position: pt,
-      draggable: false,
-      icon: icon,
-      title: 'Hi'
-    });
-  });
+  // map.on("click", function (e) {
+  //     var pt = e.latlng;
+  //     document.getElementById('test').innerHTML = pt;
+  //     var icon = L.divIcon({
+  //         className: 'my-div-icon',
+  //         // html:'<div class="popper">' + index+1 + '</div>',
+  //         html: "<img class='popper'  src=" + "'https://maps.mapmyindia.com/images/2.png'>" + '<span class="my-div-span">' + (index + 1) + '</span>',
+  //         iconSize: [2, 10],
+  //         popupAnchor: [12, -10]
+  //     });
+  //     var mk = addMarker({
+  //         position:pt,
+  //         draggable:false,
+  //         icon:icon,
+  //         title:"Hi"
+  //     });
+  // });
 });
